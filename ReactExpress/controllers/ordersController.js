b62638f9b65a9ff34dd81d5fec427e5fec1241ed
Findlay <findlay12@ymail.com>
2021-01-10 20:36:12 +0100

    fixed partly orders controller for standard lint

--- ReactExpress/controllers/ordersController.js
+++ ReactExpress/controllers/ordersController.js
@@ -3,7 +3,7 @@ const query = require('../libs/dbQuery')
 const moment = require('moment')
 
 /**
- * Fetches the order history of a user. Joins data from three tables. 
+ * Fetches the order history of a user. Joins data from three tables.
  */
 controller.fetchHistory = async (req, res, next) => {
   var email = req.body.email
@@ -14,10 +14,9 @@ controller.fetchHistory = async (req, res, next) => {
   LEFT JOIN (select sum(quantity) AS quantity_order, phone_db.product_order.order_number FROM phone_db.product_order 
     GROUP BY phone_db.product_order.order_number) AS quantities 
     ON quantities.order_number = phone_db.order.order_number;`
-    
-  //var sql = `SELECT * FROM phone_db.customer_history WHERE email = '${email}';`
+  // var sql = `SELECT * FROM phone_db.customer_history WHERE email = '${email}';`
   var result = await sendQuery(sql)
-  res.send({msg: result})
+  res.send({ msg: result })
 }
 
 /**
@@ -25,19 +24,17 @@ controller.fetchHistory = async (req, res, next) => {
  * Then inserts order into order table, followed by inserting each product into products table.
  */
 controller.insertOrder = async (req, res, next) => {
-  try{
+  try {
     await sendQuery('START TRANSACTION;')
     const customer = await handleCustomer(req)
     const order = await handleOrder(customer[0].id_customer, req.body.total)
-    
     if (orderId = order.insertId) {
       const basket = req.body.basketItems
-  
       for (const i in basket) {
         const item = basket[i]
-        if(item.quantity > 0) {
-          let result = await handleProduct(item, orderId)
-          if(result.affectedRows < 1){
+        if (item.quantity > 0) {
+          const result = await handleProduct(item, orderId)
+          if (result.affectedRows < 1) {
             throw new Error('Not enough products in stock.')
           }
         }
@@ -46,18 +43,18 @@ controller.insertOrder = async (req, res, next) => {
 
     await sendQuery('COMMIT;')
     res.send({ msg: 'Successful' })
-  }catch(err){
+  } catch (err) {
     await sendQuery('ROLLBACK;')
-    res.send(500, {error: err.message})
+    res.send(500, { error: err.message })
   }
 }
 
 /**
  *  Handles queries to database. Uses dbQuery library.
  */
-async function sendQuery(sql) {
+async function sendQuery (sql) {
   try {
-    let result = await query(sql)
+    const result = await query(sql)
     return result
   } catch (error) {
     console.log(error)
@@ -70,7 +67,7 @@ async function sendQuery(sql) {
 /**
  * Inserts customer to customer table if not available, then fetches customer id for next query.
  */
-async function handleCustomer(req) {
+async function handleCustomer (req) {
   let sql = `INSERT INTO Customer(firstname,lastname, address,state,postal_code,email)
   VALUES('${req.body.firstname}','${req.body.lastname}', '${req.body.address}', '${req.body.state}', '${req.body.zip}', '${req.body.email}')
   ON DUPLICATE KEY UPDATE
@@ -90,10 +87,10 @@ async function handleCustomer(req) {
 /**
  * Inserts order into the order table. Returns result of insert for next query.
  */
-async function handleOrder(customerID, orderTotal) {
-  let sql = `INSERT INTO Phone_Db.Order(Customer_id_customer, order_date, order_total) 
+async function handleOrder (customerID, orderTotal) {
+  const sql = `INSERT INTO Phone_Db.Order(Customer_id_customer, order_date, order_total) 
   VALUES('${customerID}', '${moment().format('YYYY-MM-D')}', ${orderTotal})`
-  result = await sendQuery(sql)
+  const result = await sendQuery(sql)
 
   return result
 }
@@ -101,16 +98,16 @@ async function handleOrder(customerID, orderTotal) {
 /**
  * Inserts product into products table.
  */
-async function handleProduct(item, orderId) {
+async function handleProduct (item, orderId) {
   // Insert with check if stock is enough.
-  let sql = `INSERT INTO Phone_Db.Product_Order(Product_id_products, quantity, order_number)
+  const sql = `INSERT INTO Phone_Db.Product_Order(Product_id_products, quantity, order_number)
   SELECT '${item.id}','${item.quantity}', '${orderId}'
   WHERE (SELECT stock FROM Phone_Db.Product WHERE id_products = ${item.id}) >= ${item.quantity}`
-  let result = await sendQuery(sql)
+  const result = await sendQuery(sql)
 
-  if(result.affectedRows > 0){
-    let removeStock = `UPDATE Phone_Db.Product SET stock = stock - ${item.quantity} WHERE id_products = ${item.id}`
-    let res = await sendQuery(removeStock)
+  if (result.affectedRows > 0) {
+    const removeStock = `UPDATE Phone_Db.Product SET stock = stock - ${item.quantity} WHERE id_products = ${item.id}`
+    await sendQuery(removeStock)
   }
 
   return result
